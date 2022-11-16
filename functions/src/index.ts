eee879e606ef1415d5a0f6f268b01b5245f67939
Gerro Den <GerroDen@users.noreply.github.com>
2022-11-16 20:23:22 +0100

    Create or update the message for a single PR by named messages

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,9 @@ import {
     signatureHeader,
     verifyBitbucketRequest,
 } from "./verify-bitbucket-request"
+import { initializeApp } from "firebase-admin/app"
 
+initializeApp()
 const spaceId = "AAAAM8DCe1U"
 
 export const bitbucketToGChat = region(config.region).https.onRequest(async (req, res): Promise<void> => {
@@ -19,21 +21,32 @@ export const bitbucketToGChat = region(config.region).https.onRequest(async (req
         res.sendStatus(403)
         return
     }
-    try {
-        const event: BitbucketEvent = bitbucketEventSchema.parse(req.body)
-        if ("test" in event) {
-            // only tests availability from the endpoint
-            res.send()
-            return
-        }
-        const reply: chat_v1.Schema$Message = {
-            text: "Hello from Firebase!",
-        }
-        const chat = google.chat("v1")
-        await chat.spaces.messages.create({ parent: `spaces/${spaceId}`, requestBody: reply })
+    const event: BitbucketEvent = bitbucketEventSchema.parse(req.body)
+    if ("test" in event) {
+        // only tests availability from the endpoint
         res.send()
-    } catch (e) {
-        console.error(e)
-        res.sendStatus(500)
+        return
     }
+    const prId = event.pullRequest.id
+    const reply: chat_v1.Schema$Message = {
+        text: `PR #${prId}: ${event.pullRequest.title}`,
+    }
+    const auth = new google.auth.GoogleAuth({
+        scopes: ["https://www.googleapis.com/auth/chat.bot"],
+    })
+    const authClient = await auth.getClient()
+    google.options({ auth: authClient })
+    const chat = google.chat("v1")
+    const parent = `spaces/${spaceId}`
+    const messageId = `client-pr-${prId}`
+    const messageName = `${parent}/messages/${messageId}`
+    const exists = await chat.spaces.messages.get({ name: messageName }).then(() => true).catch(() => false)
+    if (exists) {
+        await chat.spaces.messages.update({ name: messageName, requestBody: reply, updateMask: "text" })
+        console.debug(`updated message for PR #${prId}`)
+    } else {
+        await chat.spaces.messages.create({ parent, messageId, requestBody: reply })
+        console.debug(`created message for PR #${prId}`)
+    }
+    res.send()
 })
