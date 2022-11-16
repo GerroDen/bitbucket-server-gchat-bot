5b98f9169f4695e5e09ce057ee362564bdc6ec34
Gerro Den <GerroDen@users.noreply.github.com>
2022-11-16 16:34:44 +0100

    Implement test function to write to gchat

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,25 @@
-import {
-    logger,
-    region,
-} from "firebase-functions"
+import { region } from "firebase-functions"
 import * as config from "./config"
-import { chat_v1 } from "googleapis/build/src/apis/chat/v1"
-import { verifyGChatBearerToken } from "./verify-gchat-bearer-token"
+import {
+    chat_v1,
+    google,
+} from "googleapis"
 
-export const googleChatBot = region(config.region).https.onRequest(async (req, res): Promise<void> => {
-    const tokenVerified = await verifyGChatBearerToken(req)
-    if (!tokenVerified) {
-        res.sendStatus(401)
-        return
+export const bitbucketToGChat = region(config.region).https.onRequest(async (req, res): Promise<void> => {
+    try {
+        console.log(req.body)
+        const reply: chat_v1.Schema$Message = {
+            text: "Hello from Firebase!",
+        }
+        const auth = new google.auth.GoogleAuth({
+            scopes: ["https://www.googleapis.com/auth/chat.bot"],
+        })
+        const authClient = await auth.getClient()
+        const chat = google.chat("v1")
+        await chat.spaces.messages.create({ parent: "spaces/AAAAM8DCe1U", requestBody: reply, auth: authClient })
+        res.send()
+    } catch (e) {
+        console.error(e)
+        res.sendStatus(500)
     }
-    const body = req.body as chat_v1.Schema$DeprecatedEvent
-    logger.log(JSON.stringify(body))
-    const reply: chat_v1.Schema$Message = {
-        text: "Hello from Firebase!",
-    }
-    res.send(reply)
 })
