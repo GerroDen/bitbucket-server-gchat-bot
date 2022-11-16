00ce1660e532dc1f54807de9f6db441e86a48872
Gerro Den <GerroDen@users.noreply.github.com>
2022-11-16 20:27:07 +0100

    Move chat requests into separate function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,6 @@
 import { region } from "firebase-functions"
 import * as config from "./config"
-import {
-    chat_v1,
-    google,
-} from "googleapis"
+import { chat_v1 } from "googleapis"
 import { bitbucketEventSchema } from "./bitbucket-events-schema"
 import { BitbucketEvent } from "./bitbucket-events"
 import {
@@ -11,6 +8,7 @@ import {
     verifyBitbucketRequest,
 } from "./verify-bitbucket-request"
 import { initializeApp } from "firebase-admin/app"
+import { createOrUpdateMessage } from "./pull-request-messages"
 
 initializeApp()
 const spaceId = "AAAAM8DCe1U"
@@ -28,25 +26,13 @@ export const bitbucketToGChat = region(config.region).https.onRequest(async (req
         return
     }
     const prId = event.pullRequest.id
-    const reply: chat_v1.Schema$Message = {
+    const message: chat_v1.Schema$Message = {
         text: `PR #${prId}: ${event.pullRequest.title}`,
     }
-    const auth = new google.auth.GoogleAuth({
-        scopes: ["https://www.googleapis.com/auth/chat.bot"],
+    await createOrUpdateMessage({
+        prId,
+        spaceId,
+        message,
     })
-    const authClient = await auth.getClient()
-    google.options({ auth: authClient })
-    const chat = google.chat("v1")
-    const parent = `spaces/${spaceId}`
-    const messageId = `client-pr-${prId}`
-    const messageName = `${parent}/messages/${messageId}`
-    const exists = await chat.spaces.messages.get({ name: messageName }).then(() => true).catch(() => false)
-    if (exists) {
-        await chat.spaces.messages.update({ name: messageName, requestBody: reply, updateMask: "text" })
-        console.debug(`updated message for PR #${prId}`)
-    } else {
-        await chat.spaces.messages.create({ parent, messageId, requestBody: reply })
-        console.debug(`created message for PR #${prId}`)
-    }
     res.send()
 })
---
+++ functions/src/pull-request-messages.ts
@@ -0,0 +1,30 @@
+import {
+    chat_v1,
+    google,
+} from "googleapis"
+
+interface CreateOrUpdateMessageParams {
+    spaceId: string
+    prId: number
+    message: chat_v1.Schema$Message
+}
+
+export async function createOrUpdateMessage({ spaceId, prId, message }: CreateOrUpdateMessageParams) {
+    const parent = `spaces/${spaceId}`
+    const messageId = `client-pr-${prId}`
+    const messageName = `${parent}/messages/${messageId}`
+    const auth = new google.auth.GoogleAuth({
+        scopes: ["https://www.googleapis.com/auth/chat.bot"],
+    })
+    const authClient = await auth.getClient()
+    google.options({ auth: authClient })
+    const chat = google.chat("v1")
+    const exists = await chat.spaces.messages.get({ name: messageName }).then(() => true).catch(() => false)
+    if (exists) {
+        await chat.spaces.messages.update({ name: messageName, requestBody: message, updateMask: "text" })
+        console.debug(`updated message for PR #${prId}`)
+    } else {
+        await chat.spaces.messages.create({ parent, messageId, requestBody: message })
+        console.debug(`created message for PR #${prId}`)
+    }
+}
