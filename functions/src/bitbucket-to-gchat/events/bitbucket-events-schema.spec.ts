378f05933c175d968908c8bc934eedb4a464f851
Gerro Den <GerroDen@users.noreply.github.com>
2023-04-25 11:14:14 +0200

    Update unit tests to really test all kind of json payloads for bitbucket events

--- functions/src/bitbucket-to-gchat/events/bitbucket-events-schema.spec.ts
+++ functions/src/bitbucket-to-gchat/events/bitbucket-events-schema.spec.ts
@@ -8,67 +8,69 @@ import {
 } from "@/bitbucket-to-gchat/events/bitbucket-events-schema"
 import * as path from "path"
 
-describe("bitbucketEventSchema", () => {
-    const eventDataFiles = glob.sync("__test-data__/bitbucket-events/*.json", { cwd: __dirname })
-    for (const eventDataFile of eventDataFiles) {
-        it(`parses ${path.basename(eventDataFile)}`, async () => {
-            const eventData = await import("./" + eventDataFile)
+describe("bitbucket-events-schema", () => {
+    describe("bitbucketEventSchema", () => {
+        const eventDataFiles = glob.sync("__test-data__/*.json", { cwd: __dirname })
+        for (const eventDataFile of eventDataFiles) {
+            it(`parses ${path.basename(eventDataFile)}`, async () => {
+                const eventData = await import("./" + eventDataFile)
 
-            const result = bitbucketEventSchema.parse(eventData)
+                const result = bitbucketEventSchema.parse(eventData)
+
+                expect(eventData).toEqual(expect.objectContaining(result))
+            })
+        }
+    })
+
+    describe("pullRequestApprovalEventSchema", () => {
+        it("parses reviewer approved event", async () => {
+            const eventData = await import("@/bitbucket-to-gchat/events/__test-data__/pr-reviewer-approved.json")
+
+            const result = pullRequestApprovalEventSchema.parse(eventData)
 
             expect(eventData).toEqual(expect.objectContaining(result))
         })
-    }
-})
 
-describe("pullRequestApprovalEventSchema", () => {
-    it("parses reviewer approved event", async () => {
-        const eventData = await import("../__test-data__/bitbucket-events/pr-reviewer-approved.json")
+        it("parses reviewer needs work event", async () => {
+            const eventData = await import("@/bitbucket-to-gchat/events/__test-data__/pr-reviewer-needs-work.json")
 
-        const result = pullRequestApprovalEventSchema.parse(eventData)
+            const result = pullRequestApprovalEventSchema.parse(eventData)
 
-        expect(eventData).toEqual(expect.objectContaining(result))
+            expect(eventData).toEqual(expect.objectContaining(result))
+        })
     })
 
-    it("parses reviewer needs work event", async () => {
-        const eventData = await import("../__test-data__/bitbucket-events/pr-reviewer-needs-work.json")
+    describe("pullRequestReviewersUpdatedEventSchema", () => {
+        it("parses reviewer updated event", async () => {
+            const eventData = await import("@/bitbucket-to-gchat/events/__test-data__/pr-reviewer-updated.json")
 
-        const result = pullRequestApprovalEventSchema.parse(eventData)
+            const result = pullRequestReviewersUpdatedEventSchema.parse(eventData)
 
-        expect(eventData).toEqual(expect.objectContaining(result))
-    })
-})
-
-describe("pullRequestReviewersUpdatedEventSchema", () => {
-    it("parses reviewer updated event", async () => {
-        const eventData = await import("../__test-data__/bitbucket-events/pr-reviewer-updated.json")
-
-        const result = pullRequestReviewersUpdatedEventSchema.parse(eventData)
-
-        expect(eventData).toEqual(expect.objectContaining(result))
-    })
-})
-
-describe("pullRequestModifiedEventSchema", () => {
-    it("parses pr modified event", async () => {
-        const eventData = await import("../__test-data__/bitbucket-events/pr-modified.json")
-
-        const result = pullRequestModifiedEventSchema.parse(eventData)
-
-        expect(eventData).toEqual(expect.objectContaining(result))
-    })
-})
-
-describe("bitbucketUserTypeSchema", () => {
-    it("parses NORMAL", () => {
-        const result = bitbucketUserTypeSchema.parse("NORMAL")
-
-        expect(result).toBe("NORMAL")
+            expect(eventData).toEqual(expect.objectContaining(result))
+        })
     })
 
-    it("parses anything", () => {
-        const result = bitbucketUserTypeSchema.parse("anything")
+    describe("pullRequestModifiedEventSchema", () => {
+        it("parses pr modified event", async () => {
+            const eventData = await import("@/bitbucket-to-gchat/events/__test-data__/pr-modified.json")
 
-        expect(result).toBe("anything")
+            const result = pullRequestModifiedEventSchema.parse(eventData)
+
+            expect(eventData).toEqual(expect.objectContaining(result))
+        })
+    })
+
+    describe("bitbucketUserTypeSchema", () => {
+        it("parses NORMAL", () => {
+            const result = bitbucketUserTypeSchema.parse("NORMAL")
+
+            expect(result).toBe("NORMAL")
+        })
+
+        it("parses anything", () => {
+            const result = bitbucketUserTypeSchema.parse("anything")
+
+            expect(result).toBe("anything")
+        })
     })
 })
